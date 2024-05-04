cc7fe088f61a941e8b48e91bddc4e6afacf78598
Sif Oberon <muell758@d.umn.edu>
2024-05-04 10:44:08 -0500

    build landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
+import React from 'react'
 import './App.css'
+import { Frame } from './LandingPage'
+import { LandingPage } from './components/LandingPage'
 
 function App() {
-
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world! Test deploy!
-    </h1>
+      {/* <h1 className="text-3xl font-bold underline">
+        Hello world! Test deploy!
+      </h1> */}
+
+    <div className="App">
+      <LandingPage />
+    </div>
+
     </>
   )
 }
---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import "./LandingPage.css";
+
+export const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <div className="div">
+        <div className="landing-page-wrapper">
+          <div className="div-4">
+            <div className="div-5">
+              <div className="div-6">
+                <p className="text-wrapper">Have You Ever Struggled to Communicate Your Full Potential?</p>
+                <p className="p">
+                  more inforLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                  labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
+                </p>                  
+              </div>
+            </div>                                                                                                       
+            <div className="overlap-group-wrapper">
+              <div className="overlap-group">
+                <img className="icon" alt="Icon" src="icon.svg" />
+                <img className="unsplash-i" alt="Unsplash i" src="unsplash-i-epv9dxg7u.png" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="text-wrapper-2">What do we do?</div>
+      <div className="div-7">
+        <div className="div-8" />
+        <div className="div-9">
+          <div className="text-wrapper-3">Overcoming Imposter Syndrome</div>
+          <p className="text-wrapper-4">
+            We help early-career developers facing imposter syndrome during the job application process. We foster
+            self-assurance by helping users to identify and showcase their skills.
+          </p>
+        </div>
+      </div>
+      <div className="div-10">
+        <div className="div-11">
+          <div className="text-wrapper-5">Identifying Transferable Skills</div>
+          <p className="text-wrapper-4">
+            more inforLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
+          </p>
+        </div>
+        <div className="unsplash-wrapper">
+          <img className="unsplash" alt="Unsplash" src="unsplash-ic5f0ozntlw.png" />
+        </div>
+      </div>
+      <div className="div-12">
+        <div className="text-wrapper-6">Who is it for?</div>
+        <div className="div-13">
+          <div className="div-14" />
+          <div className="div-15">
+            <div className="div-16">
+              <div className="ellipse" />
+              <p className="text-wrapper-7">more inforLorem ipsum dolor sit amet, consectetur adipiscing</p>
+            </div>
+            <div className="div-16">
+              <div className="ellipse" />
+              <p className="text-wrapper-7">more inforLorem ipsum dolor sit amet, consectetur adipiscing</p>
+            </div>
+            <div className="div-16">
+              <div className="ellipse" />
+              <p className="text-wrapper-7">more inforLorem ipsum dolor sit amet, consectetur adipiscing</p>
+            </div>
+            <div className="div-16">
+              <div className="ellipse" />
+              <p className="text-wrapper-7">more inforLorem ipsum dolor sit amet, consectetur adipiscing</p>
+            </div>
+            <div className="div-16">
+              <div className="ellipse" />
+              <p className="text-wrapper-7">more inforLorem ipsum dolor sit amet, consectetur adipiscing</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
