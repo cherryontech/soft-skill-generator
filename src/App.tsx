fdc6cc22a9fbdb66105afaadd1e3b2d7c5b78552
adrienne365 <adrienne365@gmail.com>
2024-05-09 16:09:22 -0400

    tailwind.config updated with text styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Footer from '../src/components/Footer'
 import logo from '../src/assets/logo.jpg'
+import StyleCheck from './components/StyleCheck'
 
 function App() {
 
   return (
     <>
+    <StyleCheck />
     <Footer title={logo}/>
     </>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,26 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        inter:['inter']
+      },
+      fontSize: {
+        52:'52px',
+        48:'48px',
+        32:'32px',
+        20:'20px'
+      },
+      fontWeight: {
+        bold:'700',
+        semibold:'600',
+        regular:'400'
+      },
       colors: {
-        customYellow: '#EEF26D',
+        yellow:'#eef26d',
+        lightpurple:'#b796d9',
+        darkpurple:'#4e2973',
+        white:'#f2f2f2',
+        black:'#0d0d0d'
       },
     },
   },
