a88ad4e7fbebbbe16f4e8533c5a427361632465e
Katherine Phan <katherinephan@KatherinesM1Max.home>
2024-05-17 12:36:45 -0700

    Fixed the typo in questions and merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,8 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<LandingPage/>}/>
-        <Route path='/screenerquestion1' element={<ScreenerQuestion question={'Are you transistion in new career?'}/>}/>
-        <Route path='/screenerquestion2' element={<ScreenerQuestion question={'Are you aspiring to pursue a career as software enginnering?'}/>}/>
+        <Route path='/screenerquestion1' element={<ScreenerQuestion question={'Are you transistioning in a new career?'}/>}/>
+        <Route path='/screenerquestion2' element={<ScreenerQuestion question={'Are you aspiring to pursue a career as software enginner?'}/>}/>
       </Routes>
     <PageFooter />
   </BrowserRouter>
