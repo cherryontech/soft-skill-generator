9537025a14af45e8fd7778d28b87a5f0cf5f6320
adrienne365 <adrienne365@gmail.com>
2024-05-17 14:58:58 -0400

    removed StyleCheck component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import './App.css'
 import Footer from '../src/components/Footer'
 import logo from '../src/assets/logo.jpg'
-import StyleCheck from './components/StyleCheck'
 
 function App() {
 
   return (
     <>
-    <StyleCheck />
     <Footer title={logo}/>
     </>
   )
