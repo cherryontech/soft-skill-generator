3d7a9cab9cfdbc1e15f621239f2958b17675b2d3
Sif Oberon <muell758@d.umn.edu>
2024-05-19 10:44:17 -0500

    fix typo in screener question 2

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
       <Routes>
         <Route path='/' element={<LandingPage/>}/>
         <Route path='/screenerquestion1' element={<ScreenerQuestion question={'Are you transitioning in a new career?'}/>}/>
-        <Route path='/screenerquestion2' element={<ScreenerQuestion question={'Are you aspiring to pursue a career as software enginner?'}/>}/>
+        <Route path='/screenerquestion2' element={<ScreenerQuestion question={'Are you aspiring to pursue a career as a software engineer?'}/>}/>
       </Routes>
       <PageFooter />
     </BrowserRouter>
