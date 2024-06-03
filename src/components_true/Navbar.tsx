21f0977722867390fb1a8b26b56620f0e456c396
adrienne365 <adrienne365@gmail.com>
2024-06-03 14:07:06 -0400

    erase fixed Start Assessment buttons

--- src/components_true/Navbar.tsx
+++ src/components_true/Navbar.tsx
@@ -1,18 +1,13 @@
 import { Link, useLocation } from 'react-router-dom';
 type NavbarProps = { title: string }
-// import StyledButton from './StyledButton'
 
 function Navbar({ title }: NavbarProps) {
 
-    // const navigate = useNavigate()
     const location = useLocation()
 
-    // const handleAssessmentButton = () => {
-    //     navigate('/screenerquestion1')
-    // }
-
-    //Hide button on screener question pages
-    // const hideButtonPaths = ['/screenerquestion1', '/screenerquestion2'];
+    
+    // Hide button on screener question pages
+    const hideButtonPaths = ['/screenerquestion1', '/screenerquestion2'];
 
     return(
     <>
@@ -21,13 +16,7 @@ function Navbar({ title }: NavbarProps) {
             <Link to="/">
                 <img src={title} alt="logo" />
             </Link>
-            {/* {!hideButtonPaths.includes(location.pathname) &&  (
-                // <StyledButton 
-                //     onClick={handleAssessmentButton}
-                //     style={`bg-lightPurple rounded-lg h-12 px-4 py-2.5 text-black text-lg mr-8 md:mr-20 xl:mr-24 font-semibold leading-6`}>
-                //     Start Assessment
-                // </StyledButton>
-            )} */}
+            {!hideButtonPaths.includes(location.pathname)}
         </div>
      
       </>
