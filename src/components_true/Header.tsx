dc88942b1075ccd926a1e9d41fabfac7400e2536
Sif Oberon <muell758@d.umn.edu>
2024-06-05 19:53:15 -0500

    convert headings to sentence case

--- src/components_true/Header.tsx
+++ src/components_true/Header.tsx
@@ -14,7 +14,7 @@ function Header() {
         </div>
         <div className='bg-yellow rounded-b-lg flex md:px-24 xl:px-0 xl:flex-1'>
             <div className='flex-column py-10 px-6 px-15 xl:pl-40 xl:pr-12 xl:py-24'>
-                <h2 className='text-xl md:text-txt32 xl:text-5xl xl:leading-72 xl:w-50 md:leading-36 font-semibold mb-4 text-center xl:text-left'>Have you Ever Struggled to Communicate Your Full Potential?</h2>
+                <h2 className='text-xl md:text-txt32 xl:text-5xl xl:leading-72 xl:w-50 md:leading-36 font-semibold mb-4 text-center xl:text-left'>Have you ever struggled to communicate your full potential?</h2>
                 <p className='text-txt9 md:text-txt15 text-center xl:text-left'>At EmpowerMe, we understand the challenges you face when it comes to
                 fully showcasing your potential. Our mission is to empower you by
                 identifying and harnessing your transferable skills, helping you overcome
